55989be895f9220cf66274b5bcfa07b092caf4f5
Vardan <vardan.vanyan@gmail.com>
2023-02-18 15:57:27 -0800

    finished exercise 1

--- Day 1/day1.py
+++ Day 1/day1.py
@@ -1,3 +1,12 @@
 print("Hello World")
 # this prints to the terminal text --> strings
 
+# Exercise 1 - Printing
+# Instructions
+# Write a program in main.py that prints the same notes from the previous lesson using what you have learnt about the Python print function.
+# Warning: The output in your program should match the example output shown below exactly, character for character, even spaces and symbols should be identical, otherwise the tests won't pass.
+
+
+print("Day 1 - Python Print Function");
+print("The function is declared like this:")
+print("print('what to print')")
