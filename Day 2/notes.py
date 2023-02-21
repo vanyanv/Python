1b43f023f8b5a58a60615ab298f6d5d1f7a17491
Vardan <vardan.vanyan@gmail.com>
2023-02-21 14:23:09 -0800

    finished day2 ex1

---
+++ Day 2/notes.py
@@ -0,0 +1,16 @@
+# primitive data types
+
+# floats are decimal places
+# commas in numbers are represented with _
+# 323_604_3084
+
+
+# Booleans are true and false
+# True or False
+
+
+# type() check for the data type
+
+# type conversion
+# converting types  srt(what we want to convert) ->> converted to a string
+ 
