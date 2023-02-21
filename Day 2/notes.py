4e749b12de2ed21d92848feb2836143266858285
Vardan <vardan.vanyan@gmail.com>
2023-02-21 15:04:38 -0800

    completed exercise3 day2

--- Day 2/notes.py
+++ Day 2/notes.py
@@ -14,7 +14,12 @@
 # type conversion
 # converting types  srt(what we want to convert) ->> converted to a string
  
- 
 #  division always give a floating point number
 
 #  ** this is for exponent 6 ** 2 six to the power of two
+
+# round() ->>>> rounds up or down round(number ,you can specify the decimal places as the second argument)
+
+#  floor division (5 // 3) this does not give back a floating number or decimals coverts it into a who number chopping of the decimals does not round down or up
+
+# f string ex f"some code" allows you to incorporate multiple types does all the converting on its own
